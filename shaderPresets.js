6d438183c1e9de67b41c2649018b32f00399f33f
Bob Brugman <12188546+bbrugman@users.noreply.github.com>
2026-01-04 22:15:48 +0100

    Add Khronos PBR Neutral

--- shaderPresets.js
+++ shaderPresets.js
@@ -231,6 +231,43 @@ vec3 tonemap(vec3 x) {
     return x;
 }
 `,
+    "Khronos PBR Neutral":
+`
+// Includes code from the Khronos PBR Neutral sample GLSL implementation:
+// https://github.com/KhronosGroup/ToneMapping/blob/b5a2eed5ddf6c2227090449399de9c7affb9e4c9/PBR_Neutral/pbrNeutral.glsl
+// Licensed under the Apache License, Version 2.0:
+// https://github.com/KhronosGroup/ToneMapping/blob/main/LICENSE.md
+
+// Begin pbrNeutral.glsl
+
+// Input color is non-negative and resides in the Linear Rec. 709 color space.
+// Output color is also Linear Rec. 709, but in the [0, 1] range.
+
+vec3 PBRNeutralToneMapping( vec3 color ) {
+  const float startCompression = 0.8 - 0.04;
+  const float desaturation = 0.15;
+
+  float x = min(color.r, min(color.g, color.b));
+  float offset = x < 0.08 ? x - 6.25 * x * x : 0.04;
+  color -= offset;
+
+  float peak = max(color.r, max(color.g, color.b));
+  if (peak < startCompression) return color;
+
+  const float d = 1. - startCompression;
+  float newPeak = 1. - d * d / (peak + d - startCompression);
+  color *= newPeak / peak;
+
+  float g = 1. - 1. / (desaturation * (peak - newPeak) + 1.);
+  return mix(color, newPeak * vec3(1, 1, 1), g);
+}
+
+// End pbrNeutral.glsl
+
+vec3 tonemap(vec3 x) {
+    return PBRNeutralToneMapping(x);
+}
+`
 };
 
 for (const [key, value] of Object.entries(shaders)) {
